6315f9357bf242a82aaf4ce6716a7a31887dfd70
Harris <harris.tanweer02@gmail.com>
2022-08-04 20:26:10 -0500

    Login page Semi-Final-2

--- app/src/main/java/com/example/my_cal/MainActivity.java
+++ app/src/main/java/com/example/my_cal/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.my_cal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MaterialButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (MaterialButton) findViewById(R.id.signinbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
