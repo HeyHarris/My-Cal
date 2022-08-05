700e84447f493ad0c0647adefe72431ebb9e4247
emy1563 <73967655+emy1563@users.noreply.github.com>
2022-08-05 16:34:20 -0500

    SexBox dropdown

---
+++ app/src/main/java/com/example/my_cal/MainActivity3.java
@@ -0,0 +1,36 @@
+package com.example.my_cal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity3 extends AppCompatActivity {
+    private Spinner SexBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        SexBox= findViewById(R.id.SexBox);
+//        SexBox.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.SexBox, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        SexBox.setAdapter(adapter);
+        SexBox.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+
+
+    }
+
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {}
+        public void onNothingSelected(AdapterView<?> parent) {}
+    }
+}
+
