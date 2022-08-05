1c41d5324f50a730109068d69e20a70752572d33
HeyHarris <harris.tanweer02@gmail.com>
2022-08-05 16:40:21 -0500

    sign up page changes

--- app/src/main/java/com/example/my_cal/MainActivity.java
+++ app/src/main/java/com/example/my_cal/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
     private MaterialButton loginButton;
+    private TextView signUpLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                openActivity4();
+            }
+        });
+
+        signUpLink = (TextView) findViewById(R.id.signuplink);
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 openActivity2();
             }
         });
     }
-    public void openActivity2(){
-        Intent intent = new Intent(this, MainActivity2.class);
+    public void openActivity4(){
+        Intent intent = new Intent(this, MainActivity4.class);
         startActivity(intent);
+    }
+    public void openActivity2(){
+        Intent intentActivityTwo = new Intent(this, MainActivity2.class);
+        startActivity(intentActivityTwo);
     }
 }
