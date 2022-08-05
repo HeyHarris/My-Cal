e7bfd7f595ec21615b1861d2bf9059805bf81206
HeyHarris <harris.tanweer02@gmail.com>
2022-08-05 15:21:22 -0500

    sign up page tests

--- app/src/main/java/com/example/my_cal/MainActivity2.java
+++ app/src/main/java/com/example/my_cal/MainActivity2.java
@@ -1,14 +1,32 @@
 package com.example.my_cal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity2 extends AppCompatActivity {
+    private MaterialButton centerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        centerButton = (MaterialButton) findViewById(R.id.centerButton);
+        centerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityMain1();
+            }
+        });
+
+    }
+    public void openActivityMain1() {
+        Intent intent2 = new Intent(this, MainActivity.class);
+        startActivity(intent2);
     }
 }
