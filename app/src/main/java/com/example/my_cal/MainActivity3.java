50d674f5155a80e755370ff2bad0a7fd53d2d43d
emy1563 <73967655+emy1563@users.noreply.github.com>
2022-08-15 14:15:37 -0500

    SexBox dropdown v3 not loading

--- app/src/main/java/com/example/my_cal/MainActivity3.java
+++ app/src/main/java/com/example/my_cal/MainActivity3.java
@@ -1,36 +1,45 @@
 package com.example.my_cal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity3 extends AppCompatActivity {
-    private Spinner SexBox;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity3 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        SexBox= findViewById(R.id.SexBox);
-//        SexBox.setOnItemSelectedListener(this);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.Sexes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        SexBox.setAdapter(adapter);
-        SexBox.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        Spinner spinner = findViewById(R.id.spinner);
 
+        String[] sex = getResources().getStringArray(R.array.Sexes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sex);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity3.this, sex[i], Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
 
-    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
-        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {}
-        public void onNothingSelected(AdapterView<?> parent) {}
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
-
