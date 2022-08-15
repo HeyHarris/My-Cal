9eb0e54e3ef713148df67aea4a1d38da2aa9af99
emy1563 <73967655+emy1563@users.noreply.github.com>
2022-08-15 16:04:51 -0500

    info page done, should change spinner to dropdown https://www.geeksforgeeks.org/exposed-drop-down-menu-in-android/

---
+++ app/src/main/java/com/example/my_cal/MainActivity5.java
@@ -0,0 +1,28 @@
+package com.example.my_cal;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.my_cal.databinding.ActivityMain5Binding;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMain5Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
